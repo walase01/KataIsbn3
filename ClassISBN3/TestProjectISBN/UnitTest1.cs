cd840c63396937b9670225417a8b876092804fe9
= <=>
2021-11-25 15:36:38 -0400

    Addin code

---
+++ ClassISBN3/TestProjectISBN/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectISBN
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
