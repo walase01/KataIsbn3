349a7ec634a55cacb6c740488e2e7f47dda0a954
= <=>
2021-11-25 15:46:09 -0400

    addin code

--- ClassISBN3/ClassISBN3/ClassISBN3.cs
+++ ClassISBN3/ClassISBN3/ClassISBN3.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace ClassISBN3
 {
-    class ClassISBN3
-    {
+    public static class ClassISBN3
+    {  
+        public static bool verificarISBN(int[] numeros)
+        {
+            int multiplicacion = 0;
+            int sumatoria = 0;
+
+            if(numeros.Length == 10)
+            {
+                int posicion = 10;
+
+                for(int i = 0; i < numeros.Length; i++)
+                {
+                    multiplicacion = numeros[i] * posicion;
+                    sumatoria += multiplicacion;
+                    multiplicacion = 0;
+                    posicion--;
+                }
+
+                if(sumatoria % 11 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- ClassISBN3/TestProjectISBN/UnitTest1.cs
+++ ClassISBN3/TestProjectISBN/UnitTest1.cs
@@ -8,6 +8,12 @@ namespace TestProjectISBN
         [TestMethod]
         public void TestMethod1()
         {
+
+            int[] numeros = new int[] { 0,7,1,6,7,0,3,4,4,0};
+
+            bool resultado = ClassISBN3.ClassISBN3.verificarISBN(numeros);
+
+            Assert.AreEqual(true, resultado);
         }
     }
 }
